232dbf03b51e3149158da4adc1b1f8fa8cb78e2b
Yelyzaveta Kushneryk <kushneryk.yelyzaveta@gmail.com>
2023-12-27 17:42:43 +0100

    Add project files.

---
+++ DockerWebApi/DataContext/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DockerWebApi.Models;
+
+namespace DockerWebApi.DataContext
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        {
+            public DbSet<Platform>  Platforms { get; set; }
+    }
+    }
+}
---
+++ DockerWebApi/DataContext/IPlatformRepo.cs
@@ -0,0 +1,12 @@
+﻿using DockerWebApi.Models;
+
+namespace DockerWebApi.DataContext
+{
+    public interface IPlatformRepo
+    {
+        bool SaveChanges();
+        IEnumerable<Platform> GetAllPlatforms();
+        Platform GetPlatformById(int id);
+        void CreatePlatform(Platform platform);
+    }
+}
