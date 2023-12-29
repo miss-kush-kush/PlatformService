8b3116d94e3c87b967226b410107bcaec21e856e
Yelyzaveta Kushneryk <kushneryk.yelyzaveta@gmail.com>
2023-12-29 22:11:17 +0100

    Controller and Actions

---
+++ DockerWebApi/Controllers/PlatformsController.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using DockerWebApi.DataContext;
+using DockerWebApi.Dtos;
+using DockerWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DockerWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IPlatformRepo _repository;
+        private readonly IMapper _mapper;
+        public PlatformsController(IPlatformRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("--> Getting Platforms....");
+
+            var platformItems = _repository.GetAllPlatforms();
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<PlatformCreateDto> GetPlatformById(int id) 
+        {
+            var platformItem = _repository.GetPlatformById(id);
+            if(platformItem != null) {  return Ok(_mapper.Map<PlatformReadDto>(platformItem)); }
+            return NotFound();
+           
+        }
+
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+        {
+            var platformModel = _mapper.Map<Platform>(platformCreateDto);
+            _repository.CreatePlatform(platformModel);
+            _repository.SaveChanges();
+
+            var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+            return CreatedAtRoute(nameof(GetPlatformById), new {Id = platformReadDto.Id}, platformReadDto);
+        }
+    }
+}
--- DockerWebApi/Program.cs
+++ DockerWebApi/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(opt=>
-    opt.UseInMemoryDatabase("SuperHeroDb"));
+    opt.UseInMemoryDatabase("RAMDB"));
 
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 
