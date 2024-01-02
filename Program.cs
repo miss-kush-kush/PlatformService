e56cc5a15b359812c824fb9031653bdf49011456
miss-kush-kush <147203718+miss-kush-kush@users.noreply.github.com>
2024-01-02 22:35:14 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using PlatformService.DataContext;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opt=>
+    opt.UseInMemoryDatabase("RAMDB"));
+
+builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+PrepDb.PrepPopulation(app);
+
+app.Run();
